7d43279f10458660c3b1c686f9989dcdbaf24675
rajasekarcloud <rajasekar.unix@yahoo.com>
2025-04-13 21:56:34 -0700

    Tested

--- character_transfrom.py
+++ character_transfrom.py
@@ -10,7 +10,29 @@ def transform_message(message, cipher):
             tmsg = tmsg+cipher.get(i)
         else:
             tmsg = tmsg + i
-    print(tmsg)
+    return (tmsg)
+def transform_message_1(test_encoded,decoder):
+    result = ''
+    for char in test_encoded:
+        if char.isalpha():
+            if char.isupper():
+                upper_key=char
+                result = result + decoder.get(upper_key)
+            else:
+                lower_key=char
+                result = result + decoder.get(lower_key)
+        elif char == ",":
+            result = result + ","
+        elif char == ".":
+            result = result + "."
+        else:
+            result = result + " "
+    return result
 
 test = "I come to bury Caesar, not to praise him."
-transform_message(test,cipher)
+test_encoded=str(transform_message(test,cipher))
+test_encoded='F Zljb ql Yrov zXbpXo, klq ql moXfpb efj.'
+print(cipher)
+decoder = {v: k for k, v in cipher.items()}
+print(decoder)
+print(transform_message_1(test_encoded, decoder))
